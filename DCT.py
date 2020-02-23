544d9ae334394354513c9cbd192381848a5a8016
mdiamantino <mdcaribe@mail.com>
2020-02-23 19:41:39 +0100

    can decode (static method) from file

--- DCT.py
+++ DCT.py
@@ -35,7 +35,10 @@ class DCT:
         is shorter than the maximum available space in the image.
         Warning is raised if the message length is > (10% of the available capacity).
         """
-        tot_blocks = self.__height_ * self.__width_ // 64  # 64 -> since each quantized block is a matrix of 8x8
+        area = self.__height_ * self.__width_
+        if area < 64:  # 64 -> since each quantized block is a matrix of 8x8
+            raise ValueError("The chosen cover image is too small (area < 64 px)")
+        tot_blocks = self.__height_ * self.__width_ // 64
         if self.__message_length_ > tot_blocks:
             raise OverflowError("Cannot embed. Message is too long!")
         elif self.__message_length_ > tot_blocks / 10:
@@ -59,30 +62,34 @@ class DCT:
 
     # BREAK/RECOMPOSE METHODS ============================================================
 
-    def breakImageIntoBlocks(self, img, height, width):
+    @staticmethod
+    def breakImageIntoBlocks(img, height, width):
         """
         Breaks the coverimage into a sequence of 8x8 blocks,
         from top left to bottom right.
         :param img: Coverimage to break into n 8x8 blocks.
         :return: List of blocks of pixels [LIST OF NUMPY NDARRAY]
         """
+        if not (isinstance(img, np.ndarray)):
+            raise TypeError("Cannot break a non np.array image")
         return [img[j: j + 8, i: i + 8] for (j, i) in
                 itertools.product(range(0, height, 8),
                                   range(0, width, 8))]
 
-    def recomposeImage(self, block_size=8):
+    def recomposeImage(self):
         """
-        Inverse of 'breakImageIntoBlocks'.
-        Builds image from sorted list of blocks of pixels.
-        :param block_size: Side length of a block of pixels [INT]
-        :return: Original image [NUMPY NDARRAY]
+        Inverse of method 'breakImageIntoBlocks':
+        Recompose the image from the sorted list of blocks of pixels '__block_list_'.
+        :return: Re-composed image [NUMPY NDARRAY]
         """
-        full_image = np.zeros(shape=(self.__height_, self.__width_), dtype=np.uint8)
-        for i in range(len(self.__block_list_)):
-            curr_col_index = block_size * (i % (self.__width_ // block_size))
-            curr_line_index = block_size * (i // (self.__width_ // block_size))
-            full_image[curr_line_index:curr_line_index + block_size,
-            curr_col_index:curr_col_index + block_size] = self.__block_list_[i]
+        if len(self.__block_list_) == 0:
+            raise ValueError
+        full_image = np.zeros(shape=(self.__height_, self.__width_), dtype=np.uint8)  # Builds empty image
+        for i in range(len(self.__block_list_)):  # Filling image
+            curr_col_index = 8 * (i % (self.__width_ // 8))
+            curr_line_index = 8 * (i // (self.__width_ // 8))
+            full_image[curr_line_index:curr_line_index + 8,
+            curr_col_index:curr_col_index + 8] = self.__block_list_[i]
         return full_image
 
     # QUANTIZATION METHODS ===============================================================
@@ -90,11 +97,11 @@ class DCT:
     @staticmethod
     def getQuantizedBlock(block):
         """
-        Centers values of a block, runs it through DCT func and quantizes it.
+        Centers values of a block, runs it through DCT func. and quantizes it.
         :param block: 8x8 block of pixels [NUMPY NDARRAY]
         :return:Quantized 8x8 block of pixels [NUMPY NDARRAY]
         """
-        img_block = (np.subtract(block, 128))
+        img_block = np.subtract(block, 128)
         dct_block = cv2.dct(img_block.astype(np.float64))
         dct_block[0][0] /= settings.QUANTIZATION_TABLE[0][0]
         dct_block[0][0] = np.round(dct_block[0][0])
@@ -116,15 +123,13 @@ class DCT:
 
     def lengthToBinary(self):
         """
-        Gives binary form of the length and adds a separator to it.
-        :return: Bits of the length + separator to embed [LIST OF STR]
+        Gives binary form of the length and adds a separator to that representation.
+        :return: Binary representation of the length + separator to embed [LIST OF STR]
         """
         assert self.__message_length_ % 8 == 0
-        msg_length = int(self.__message_length_ / 8)
+        msg_length = int(self.__message_length_ / 8)  # Decimal representation of the length
         n_required_bits = msg_length.bit_length()
-        if n_required_bits % 8 != 0:
-            n_required_bits = (1 + (n_required_bits // 8)) * 8
-        tmp = "0{}b".format(n_required_bits)
+        tmp = f"0{n_required_bits}b"
         binary_length = format(msg_length, tmp)
         return list(binary_length) + utils.stringToBinary(settings.LENGTH_MSG_SEPARATOR)
 
@@ -132,8 +137,8 @@ class DCT:
         """
         Inserts the length of the message and end symbol (in binary form)
         at the beginning of the picture.
-        :return: Image with embedded length, as list of blocks of pixels
-        (from top left to bottom right) [LIST OF NUMPY NDARRAY]
+        At the end, '__block_list_' will be the image with embedded length,
+        as list of blocks of pixels (from top left to bottom right) [LIST OF NUMPY NDARRAY]
         """
         mess_len_to_binary = self.lengthToBinary()
         for block_index in range(len(mess_len_to_binary)):
@@ -146,13 +151,6 @@ class DCT:
                 quantized_block[0][0] += 1
             self.__block_list_[block_index] = self.getOriginalBlockFromQuantized(
                 quantized_block)
-        return self.__block_list_
-
-    @staticmethod
-    def getBlockFromBlockIndex(img, width, block_index, block_size=8):
-        i = 8 * (block_index % (width // block_size))
-        j = 8 * (block_index // (width // block_size))
-        return img[j:j + 8, i:i + 8]
 
     @staticmethod
     def extractMsglength(img, width):
@@ -164,30 +162,22 @@ class DCT:
         """
         block_index, curr_bit = 0, 0
         separator_found = False
-        letter_buffer = ""
-        message_len = list()
+        decoded_length = ""
         while (not separator_found) and (block_index < settings.MAX_BITS_TO_ENCODE_LENGTH):
-            block = DCT.getBlockFromBlockIndex(img, width, block_index)
+            block = utils.getBlockFromBlockIndex(img, width, block_index)
             unquantized_block = DCT.getQuantizedBlock(block)
-            letter_buffer += str(int(unquantized_block[0][0] % 2))
-
-            curr_bit += 1
-            if curr_bit == 8:
-                current_letter = str(chr(int(letter_buffer, 2)))
-                curr_bit = 0
+            decoded_length += str(int(unquantized_block[0][0] % 2))
+            if len(decoded_length) > 8:
+                current_letter = str(chr(int(decoded_length[-8:], 2)))
                 if current_letter == settings.LENGTH_MSG_SEPARATOR:
                     separator_found = True
-                else:
-                    message_len.append(letter_buffer)
-                letter_buffer = ""
             block_index += 1
-        binary_len = [item for sublist in message_len for item in sublist]
-        return int(''.join(binary_len), 2) * 8
+        return int(''.join(decoded_length[:-8]), 2) * 8
 
     # ENCODE/DECODE MESSAGE METHODS =======================================================
 
     @staticmethod
-    def getRandomBlocksFromMsglength(seed, binary_msg_length, height, width):
+    def getRandomBlocksFromMsgLength(seed, binary_msg_length, height, width):
         """
         Generates a random sequence of indices, (interpreted as the position
         of bits of the message to embed/extract in/from the cover/stegoimage.
@@ -206,25 +196,23 @@ class DCT:
 
     def encode_r(self, output_path, seed):
         """
-        Embed a message into a cover image:
+        Embed message into cover image:
         1 - Embed the length.
         2 - Embed the message equally distributing bits according
-            to a random sequence of indeces
+            to a random sequence of indices
             and inserting them in the LSB of DCT quantized blocks.
         :param output_path: Path of stegoimage that will be generated
                             after message insertion [STR]
         :param seed: Chosen seed [INT]
         :return: Stegoimage [NUMPY NDARRAY]
         """
-        img = self.__cover_image_
-        YCrCbImage = cv2.cvtColor(self.__cover_image_, cv2.COLOR_BGR2YCR_CB)
-        y, cr, img = cv2.split(YCrCbImage)
+        y, cr, cb = utils.getYCrCbFromOriginalImg(self.__cover_image_)
         mess_len = len(self.__cipher_text_)
-        dic = self.getRandomBlocksFromMsglength(seed, mess_len * 8,
+        dic = self.getRandomBlocksFromMsgLength(seed, mess_len * 8,
                                                 self.__height_, self.__width_)
-        self.__block_list_ = self.breakImageIntoBlocks(img, self.__height_,
+        self.__block_list_ = self.breakImageIntoBlocks(cb, self.__height_,
                                                        self.__width_)
-        self.__block_list_ = self.embedMsglength()
+        self.embedMsglength()
         for message_index in range(len(dic)):
             block_index = dic[message_index]
             block = self.__block_list_[block_index]
@@ -239,36 +227,34 @@ class DCT:
 
             self.__block_list_[block_index] = self.getOriginalBlockFromQuantized(
                 dct_block)
-        rgb_final_img = self.recomposeImage()
-        final_img = cv2.merge((y, cr, rgb_final_img))
+        cb = self.recomposeImage()
+        final_img_standard_format = utils.getOriginalImgFromYCrCb(y, cr, cb)
+        cv2.imwrite(output_path, final_img_standard_format)
+        return final_img_standard_format
 
-        cv2.imwrite(output_path, cv2.cvtColor(final_img, cv2.COLOR_YCR_CB2BGR))
-        return cv2.cvtColor(final_img, cv2.COLOR_YCR_CB2BGR)
-
-    def decode_r(self, original_stego_img, seed):
+    @staticmethod
+    def decode_r(setego_img_path, seed):
         """
         Extract a message from a stegoimage:
         1 - Extract the length of the message.
-        2 - Generate the random sequence of indeces.
+        2 - Generate the random sequence of indices.
         3 - Extract the message.
         :param stego_img_path: Path of stegoimage
                                from which to extract the message [STR]
         :param seed: Chosen seed [INT]
-        :return: Message hiddent in the stegoimage [STR]
+        :return: Message hidden in the stegoimage [BYTE STR]
         """
-        # original_stego_img = cv2.imread(stego_img_path, cv2.IMREAD_GRAYSCALE)
-
+        original_stego_img = utils.getImage(setego_img_path)
+        y, cr, cb = utils.getYCrCbFromOriginalImg(original_stego_img)
         height, width = original_stego_img.shape[:2]
-        YCrCbImage = cv2.cvtColor(original_stego_img, cv2.COLOR_BGR2YCR_CB)
-        y, cr, img = cv2.split(YCrCbImage)
-        msg_length = DCT.extractMsglength(img, width)
-        dic = DCT.getRandomBlocksFromMsglength(seed, msg_length, height, width)
+        msg_length = DCT.extractMsglength(cb, width)
+        dic = DCT.getRandomBlocksFromMsgLength(seed, msg_length, height, width)
         decoded_msg = "0b"
-        block_list = self.breakImageIntoBlocks(img, height, width)
+        block_list = DCT.breakImageIntoBlocks(cb, height, width)
         for message_index in range(len(dic)):
             block_index = dic[message_index]
             block = block_list[block_index]
-            dct_block = self.getQuantizedBlock(block)
+            dct_block = DCT.getQuantizedBlock(block)
             coeff = int(dct_block[0][0])
             decoded_msg += str(coeff % 2)  # Adding to the message the currently read bit
         return BitArray(decoded_msg).bytes
@@ -280,7 +266,7 @@ if __name__ == "__main__":
     message = "HELLO THIS IS A LONG MESSAGE"
     encrypted = encryptMessage(message, "password")
     d = DCT("data/test_image.jpg", encrypted)
-    encoded = d.encode_r("data/ycrcb_output.jpg", 20)
-    decoded = d.decode_r(encoded, 20)
+    encoded = d.encode_r("data/ycrcb_output.png", 20)
+    decoded = DCT.decode_r("data/ycrcb_output.png", 20)
     decoded_message = decryptMessage(decoded, "password")
     print(decoded_message)
--- utils.py
+++ utils.py
@@ -24,3 +24,21 @@ def stringToBinary(string_text):
     map_of_binary_str = map(lambda x: str(format(ord(x), '08b')),
                             string_text)
     return [item for sublist in map_of_binary_str for item in sublist]
+
+
+def getBlockFromBlockIndex(img, width, block_index, block_size=8):
+    i = 8 * (block_index % (width // block_size))
+    j = 8 * (block_index // (width // block_size))
+    return img[j:j + 8, i:i + 8]
+
+
+def getYCrCbFromOriginalImg(img):
+    YCrCbImage = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    y, cr, cb = cv2.split(YCrCbImage)
+    return (y, cr, cb)
+
+
+def getOriginalImgFromYCrCb(y, cr, cb):
+    ycrcb_format_img = cv2.merge((y, cr, cb))
+    standard_format_img = cv2.cvtColor(ycrcb_format_img, cv2.COLOR_YCR_CB2BGR)
+    return standard_format_img
