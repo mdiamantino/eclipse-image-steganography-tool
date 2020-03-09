6ab80ce201288a7474e13946abf7ab1ca2eadf00
mdc <mdcaribe@mail.com>
2020-03-09 23:11:43 +0100

    adapted with cupy

--- POBA_GA.py
+++ POBA_GA.py
@@ -2,49 +2,46 @@ from random import random
 
 import cv2
 import efficientnet.keras as efn
-import numpy as np
+# import numpy as np
+# from numba import cuda
+import cupy as cp
 from keras.applications.inception_v3 import preprocess_input, decode_predictions
 
 
 # ============================================================
 
 class POBA_GA:
-    def __init__(self, image_path, T, N, alpha=3, P_c=1, P_m=0.1):
-        # self.app = ClarifaiApp(api_key='7c87e299629e4e7ea0566aca3136b214')
-        # self.model = self.app.public_models.general_model
-        self.model = efn.EfficientNetB0(weights='imagenet')  # or weights='noisy-student'
-        self.S = cv2.imread(image_path)
+    def __init__(self, model, image_path, T, N, alpha=2, P_c=1, P_m=0.1):
+        self.model = model
+        self.S = cp.array(cv2.imread(image_path))
         self.height, self.width = self.S.shape[0], self.S.shape[1]
+        self.img_area = self.height * self.width
         self.maxZ_A_t0 = None
         self.T = T
         self.N = N
         self.alpha = alpha
         self.P_c, self.P_m = P_c, P_m
-        self.fs = np.zeros((N))
-        self.frs = np.zeros((N))
-        self.phis = np.zeros((N))
+        self.fs = cp.zeros((N))
+        self.frs = cp.zeros((N))
+        self.phis = cp.zeros((N))
         self.gamma = 0.1  # TODO
         self.A, self.AS = None, None
         self.y_0, self.p_0, _, _ = self.getLables(self.S)  # True label of the adversarial image
-        # self.y_0, self.p_0 = "tree", 0.92
         # Parameters to adjust the perturbation pixel mapping rule (if naked eye, pm1=10, pm2=5.8)
         self.pm1, self.pm2 = 10, 5.8
         self.mean, self.variance = 0, 0.5
         self.attacked = False
+        self.A = cp.zeros((2, self.N, *self.S.shape[:2]))
+        self.AS = cp.zeros((self.N, *self.S.shape[:2], 3))
         self.initialization()
-        print("ok")
 
     def initialization(self):
-        random_noises_A = []
-        for i in range(self.N):
-            random_noises_A.append(
-                self.generateRandomNoise(self.S.shape[:2], mean=self.mean, var=self.variance))
-        self.A = np.array(random_noises_A)
-        self.A.resize((2, self.N, *self.S.shape[:2]), refcheck=False)  # Add one dimension
-        self.AS = np.zeros((self.N, *self.S.shape[:2], 3))
+        print("\n[*] STARTING INITIALIZATION")
+        random_noises_A = [self.generateRandomNoise(self.S.shape[:2]) for _ in range(self.N)]
+        self.A[0] = cp.array(random_noises_A)
         self.maxZ_A_t0 = max(self.Z(i) for i in range(len(self.A[0])))
-        # print(self.maxZ_A_t0)
-        # self.mergeImgPlusNoise(t=0)
+        print(self.maxZ_A_t0)
+        print("[*] ENDING INITIALIZATION\n")
 
     def getLables(self, np_img):
         """success, encoded_image = cv2.imencode('.png', np_img)
@@ -58,8 +55,8 @@ class POBA_GA:
         y_2, p_2 = concepts[1]['name'], concepts[1]['value']
         """
         # print("-> PREDICTING")
-        x = cv2.resize(np_img, dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
-        x = np.expand_dims(x, axis=0)
+        x = cv2.resize(cp.asnumpy(np_img), dsize=(224, 224), interpolation=cv2.INTER_CUBIC)
+        x = cp.expand_dims(x, axis=0)
         x = preprocess_input(x)
         preds = self.model.predict(x)
         top2 = decode_predictions(preds, top=2)[0]
@@ -71,10 +68,9 @@ class POBA_GA:
         #     print(p_1)
         return y_1, p_1, y_2, p_2
 
-    @staticmethod
-    def generateRandomNoise(shape, mean, var):
-        sigma = var ** 0.5
-        gaussian = np.random.normal(mean, sigma, shape)
+    def generateRandomNoise(self, shape):
+        sigma = self.variance ** 0.5
+        gaussian = cp.random.normal(self.mean, sigma, shape)
         return gaussian
 
     def mergeImgPlusNoise(self):
@@ -90,32 +86,29 @@ class POBA_GA:
         :param N: Population size
         :return:
         """
-        optimal_t = 0
         for t in range(self.T):
             print("Starting iteration n°", t)
-            # Collection of the t_th iteration adversarial examples
             self.mergeImgPlusNoise()
-            self.phis = np.array([self.phi(i) for i in range(self.N)])
+            self.phis = cp.array([self.phi(i) for i in range(self.N)]) # TODO: FIX BUG
             if self.attacked:
-                break
-            tot = np.sum(self.phis)
-            self.fs = np.divide(self.phis, tot)
-            self.frs = np.array([np.sum(self.fs[:i + 1]) for i in range(len(self.fs))])
+                print("MAX---> ", cp.max(self.phis))
+                # break
+            self.fs = cp.divide(self.phis, cp.sum(self.phis))
+            self.frs = cp.array([cp.sum(self.fs[:i + 1]) for i in range(len(self.fs))])
             # if max(self.phis) > self.gamma:
             #     optimal_t = t
             #     break
             for n in range(self.N // 2):
                 i = self.selection()  # First chosen parent
                 j = self.selection()  # Second chosen parent
-                # assert i != j
                 A_2n_1_t_plus_1, A_2n_t_plus_1 = self.crossover(self.A[0][i],
                                                                 self.A[0][j])
                 self.A[1, (2 * n)] = self.mutation(A_2n_1_t_plus_1)
                 self.A[1, ((2 * n) + 1)] = self.mutation(A_2n_t_plus_1)
             self.A[0] = self.A[1]
-            self.A[1] = np.zeros(self.A.shape[1:])
+            self.A[1] = cp.zeros(self.A.shape[1:])
 
-        optimal_img_indx = np.argmax(self.phis)
+        optimal_img_indx = cp.argmax(self.phis)
         optimal_res = self.AS[optimal_img_indx]
         print(optimal_res.tolist())
         return optimal_res
@@ -127,11 +120,11 @@ class POBA_GA:
         :param A_i_t:  The tth iteration of the ith ith perturbation
         :return: Perturbation size
         """
-        var = (-np.abs(self.AS[i]) * self.pm1 + self.pm2)
-        first_exp = np.exp(var)
-        second_exp = np.exp(self.pm2)
+        var = (-cp.abs(self.A[0,i]) * self.pm1 + self.pm2)
+        first_exp = cp.exp(var)
+        second_exp = cp.exp(self.pm2)
         perturbation_size = (1 / (1 + first_exp) - 1 / (1 + second_exp))
-        return np.sum(perturbation_size)
+        return cp.sum(perturbation_size)/self.img_area
 
     def phi(self, i):
         """
@@ -143,11 +136,12 @@ class POBA_GA:
         y_1, p_1, y_2, p_2 = self.getLables(current_adversarial_img)
         if y_1 != self.y_0:
             print("----> Different LABELS")
-            print("DIFFERENT LABELS : %s != %s" % (y_1, y_2))
+            print("DIFFERENT LABELS : %s != %s" % (y_1, self.y_0))
             self.attacked = True
             res = p_1 - self.p_0 - ((self.alpha / self.maxZ_A_t0) * self.Z(i))
         else:
             res = p_2 - self.p_0
+        print(res)
         return res
 
     def f(self, i):
@@ -191,7 +185,7 @@ class POBA_GA:
         # A__t[i], A__t[j] are two parent perturbations
 
         # two-dimensional matrix in crossover
-        B = np.random.randint(2, size=(self.height, self.width))
+        B = cp.random.randint(2, size=(self.height, self.width))
         if random() < self.P_c:
             # print("ok")
             b_opposite = (1 - B)
@@ -211,7 +205,7 @@ class POBA_GA:
         :return:
         """
         # two-dimensional matrix in crossover
-        C = np.random.randint(3, size=(self.height, self.width))
+        C = cp.random.randint(3, size=(self.height, self.width))
         if random() < self.P_m:
             return A_i_t_plus_1 * C
         else:
@@ -219,5 +213,8 @@ class POBA_GA:
 
 
 if __name__ == "__main__":
-    poba_ga = POBA_GA("data/test_image.jpg", 5000, 20)
+    # self.app = ClarifaiApp(api_key='7c87e299629e4e7ea0566aca3136b214')
+    # self.model = self.app.public_models.general_model
+    model = efn.EfficientNetB0(weights='imagenet')  # or weights='noisy-student'
+    poba_ga = POBA_GA(model, "data/test_image.jpg", 5000, 20)
     poba_ga.main()
