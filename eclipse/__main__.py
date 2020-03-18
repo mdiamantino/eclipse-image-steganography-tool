5380fa05edc2323c8ff60cfa96398eaf255e2ccd
mdc <mdcaribe@mail.com>
2020-03-18 10:16:47 -0700

    fixed unmatched parenthesis

--- eclipse/__main__.py
+++ eclipse/__main__.py
@@ -2,7 +2,7 @@
 
 Usage:
   __main__.py --interactive
-  __main__.py hide --image [--stealthy] <image-path> --message <message-txt> --code <seed> --output <path>
+  __main__.py hide [--stealthy] --image <image-path> --message <message-txt> --code <seed> --output <path>
   __main__.py extract [--stealthy | --output <path>] --image <image-path> --code <seed>
   __main__.py (-h | --help)
   __main__.py --version
@@ -13,7 +13,7 @@ Options:
   -m <image-path>, --message <message-txt>  Message to hide.
   -c <seed>, --code <seed>      Repartition code used to retrieve the message.
   -o <path>, --output <path>    Path of the output stegoimage/extracted text.
-  -s, --stealthy                Stealthy mode: bury traces (original img, cover img)
+  -s, --stealthy                Stealthy mode: bury traces -original img, cover img-
   -h, --help    Show this screen.
   --version     Show version.
 """
@@ -25,5 +25,5 @@ from eclipse.ui.main_cli import main
 if __name__ == "__main__":
     f = Figlet(font='isometric3')
     print(f.renderText('HIDE'))
-    arguments = docopt(__doc__, version='DEMO 1.0')
+    arguments = docopt(__doc__, version='Eclipse 0.1')
     main(arguments)
